7c714a564bb482523e5e50ede03908c7f65c6a14
Pedro Maia <pedroabmaia@hotmail.com>
2024-10-08 17:32:53 +0100

    #1 Finished controll of the ciberrato1

--- pClient/mainRob.py
+++ pClient/mainRob.py
@@ -27,7 +27,7 @@ class PIDController:
 class MyRob(CRobLinkAngs):
     def __init__(self, rob_name, rob_id, angles, host):
         CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
-        self.pid_controller = PIDController(1.5, 0.0, 0.2)
+        self.pid_controller = PIDController(2.0, 0.0, 0.75)
         self.last_time = time.time()
 
     # In this map the center of cell (i,j), (i in 0..6, j in 0..13) is mapped to labMap[i*2][j*2].
@@ -87,7 +87,9 @@ class MyRob(CRobLinkAngs):
         left_id = 1
         right_id = 2
         back_id = 3
-        base_velocity = 0.1
+        base_velocity = 0.15
+        danger_walls = 1.8
+        danger_front = 0.95
 
         current_time = time.time()
         dt = current_time - self.last_time
@@ -96,23 +98,37 @@ class MyRob(CRobLinkAngs):
         if dt <= 0:
             dt = 0.01
 
+        front_proximity = self.measures.irSensor[center_id]
         left_proximity = self.measures.irSensor[left_id]
         right_proximity = self.measures.irSensor[right_id]
+
+        print('Front: '+str(front_proximity)+' Left: '+str(left_proximity)+' Right: '+str(right_proximity)+' Colision: '+str(self.measures.collision))
 
         error = right_proximity-left_proximity
 
         pid_output = self.pid_controller.compute(error, dt)
-        print(pid_output)
+        #print(pid_output)
 
-        if error > 1:
-            print('Rotate left')
-            self.driveMotors(max(base_velocity - pid_output,-base_velocity),+base_velocity)
-        elif error < -1:
-            print ('Rotate right')
-            self.driveMotors(+base_velocity,max(base_velocity+pid_output,-base_velocity))
-        else:
-            print('Go')
+        if (left_proximity < danger_walls) & (right_proximity < danger_walls) & (front_proximity < danger_front): # Crossways logic
+            print('Crossway')
             self.driveMotors(base_velocity,base_velocity)
+        elif front_proximity > danger_front:
+            if error > 0:
+                print('Sharp left')
+                self.driveMotors(max(base_velocity - pid_output,-base_velocity),+base_velocity)
+            elif error < 0:
+                print ('Sharp right')
+                self.driveMotors(+base_velocity,max(base_velocity+pid_output,-base_velocity))
+        else:
+            if error > 1.0:
+                print('Smooth left')
+                self.driveMotors(max(base_velocity - pid_output,-0),+base_velocity)
+            elif error < -1.0:
+                print ('Smooth right')
+                self.driveMotors(+base_velocity,max(base_velocity+pid_output,-0))
+            else:
+                print('Go')
+                self.driveMotors(base_velocity,base_velocity)
 
 
 class Map():
